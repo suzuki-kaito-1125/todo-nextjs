54a88232c3ccbe345ab1eefd23703a525d2300ec
鈴木 海斗 <kaito.suzuki@A333.local>
2024-03-15 20:12:18 +0900

    フォーム実装

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -3,14 +3,30 @@
 import { useState } from "react";
 
 export default function Todo() {
+  const [todoText, setTodoText] = useState("");
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoText(e.target.value);
+  };
+  const onClick = () => {
+    setTodoText("");
+  };
+
   return (
     <div className="p-4 space-y-2">
       <div>
         <h1 className="text-2xl">Todos</h1>
       </div>
       <div className="space-x-2">
-        <input className="text-black p-2" placeholder="TODOを入力" />
-        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <input
+          className="text-black p-2"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChange}
+        />
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={onClick}
+        >
           input
         </button>
       </div>
