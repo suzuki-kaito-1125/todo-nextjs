e71621815bc4321a99af0c10ccc5700c8958b2a8
鈴木 海斗 <kaito.suzuki@A333.local>
2024-03-15 20:00:34 +0900

    todosディレクトリのページ作成(仮デザイン)

---
+++ src/app/todos/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Todo() {
+  return (
+    <div className="p-4 space-y-2">
+      <div>
+        <h1 className="text-2xl">Todos</h1>
+      </div>
+      <div className="space-x-2">
+        <input className="text-black p-2" placeholder="TODOを入力" />
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          input
+        </button>
+      </div>
+      <div className="border-2 border-white rounded-lg p-4">
+        <ul>
+          <h2 className="text-xl">unmarked_todos</h2>
+          <li>
+            <p>Todo1</p>
+          </li>
+          <li>
+            <p>Todo1</p>
+          </li>
+        </ul>
+      </div>
+      <div className="border-2 border-white rounded-lg p-4">
+        <ul>
+          <h2 className="text-xl">marked_todos</h2>
+          <li>
+            <p>Todo1</p>
+          </li>
+          <li>
+            <p>Todo1</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
